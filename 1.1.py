3b5c08063c9145a3ea1f777a6b9c0bc54f274791
TUtochk1ya <dmitry.glushchenko@mail.ru>
2025-01-15 00:31:38 +0300

    Лекция. Функция ввода и работа с отладчиком.
    Практическое задание.
    Напишите программу, которая запрашивает у пользователя его имя и возраст
    и выводит сообщение о том, сколько лет исполнится пользователю через 5 лет.

---
+++ 1.1.py
@@ -0,0 +1,8 @@
+
+
+name = input("Введите ваше имя: ")
+age = input("Введите ваш возраст: ")
+ears = int(age) + 5
+print(f"Имя: {name}")
+print(f"Возраст: {age}")
+print(f"Через 5 лет вам будет: {ears}")
