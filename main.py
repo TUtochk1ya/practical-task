ff955e2dc4f0a0ca3342724888bd24e53e6a1dbb
TUtochk1ya <dmitry.glushchenko@mail.ru>
2025-01-22 14:54:58 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 
 
 # 1st program
+
 print((9**0.5)*5)
 
 # 2st program
+
 print(9.99>9.98 and 1000!= 1000.1)
 
 # 3st program
+
 print(2*2+2)
 print(2*(2+2))
 print((2*2+2)==(2*(2+2)))
 
 # 4th program
+
 print("123.456")
 print(float("123.456"))
 print(123.456*10)
 print(int(1234.56))
-print(1234/10)
+print(float(1234/10))
